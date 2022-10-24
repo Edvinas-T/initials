283d2f4cc55f1e870e1904f5b27f1e3aa0ae48e1
STUDENT Edvinas Tkacenka <Edvinas.Tkacenka@students.ittralee.ie>
2022-10-24 15:50:52 +0100

    Signed-off-by: STUDENT Edvinas Tkacenka <Edvinas.Tkacenka@students.ittralee.ie>

---
+++ GraphicsPipeline/Assets/Outcode.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outcode
+{
+
+    bool UP, DOWN, LEFT, RIGHT;
+
+    public Outcode(Vector2 v)
+    {
+        UP = (v.y > 1);
+        RIGHT = (v.x > 1);
+        DOWN = (v.y < -1);
+        LEFT = (v.x < -1);
+        
+    }
+    public Outcode(bool up, bool down, bool left, bool right)
+    {
+        UP = up;
+        DOWN = down;
+        RIGHT = right;
+        LEFT = left;
+    }
+
+    public Outcode()
+    {
+        UP = false;
+        DOWN = false;
+        RIGHT = false;
+        LEFT = false;
+    }
+
+    public static Outcode operator * (Outcode A, Outcode B)
+    {
+        return new Outcode(A.UP && B.UP, A.DOWN && B.DOWN, A.LEFT && B.LEFT, A.RIGHT && B.RIGHT);
+    }
+    public static Outcode operator +(Outcode A, Outcode B)
+    {
+        return new Outcode(A.UP || B.UP, A.DOWN || B.DOWN, A.LEFT || B.LEFT, A.RIGHT || B.RIGHT);
+    }
+    public static bool operator ==(Outcode A, Outcode B)
+    {
+        return (A.UP == B.UP)&& (A.DOWN == B.DOWN) && (A.LEFT == B.LEFT) && (A.RIGHT == B.RIGHT);
+    }
+
+    public static bool operator !=(Outcode A, Outcode B)
+    {
+        return !(A == B);
+    }
+
+    public void print(Outcode A)
+    {
+        string O = (UP ? "1" : "0") + (DOWN ? "1" : "0") + (LEFT ? "1" : "0") + (RIGHT ? "1" : "0");
+        Debug.Log(O);
+        
+    }
+
+
+
+}
--- GraphicsPipeline/Assets/graphicsPipeline.cs
+++ GraphicsPipeline/Assets/graphicsPipeline.cs
@@ -6,10 +6,17 @@ public class graphicsPipeline : MonoBehaviour
 {
     
     Model d;
+    Outcode A = new Outcode(new Vector2(-2, -2));
+    Outcode B = new Outcode(new Vector2(2, -2));
+    Outcode C = new Outcode(new Vector2(0.2f, 0.1f));
+
     void Start()
     {
         d = new Model();
-     
+
+        A.print(A);
+        B.print(B);
+        C.print(C);
     }
 
     // Update is called once per frame
